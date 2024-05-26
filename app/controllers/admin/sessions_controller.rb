1522ffe131eeb5add5d090cd9afdfc6c527adc6e
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-05-26 14:56:52 +0000

    [TakuHome]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,18 +19,18 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
+  # protected
   
-  def after_sign_in_path_for(resource)
-    admin_dashboards_path # ログイン後にリダイレクトするパス
-  end
+  # def after_sign_in_path_for(resource)
+    # admin_dashboards_path # ログイン後にリダイレクトするパス
+  # end
 
-  def after_sign_out_path_for(resource_or_scope)
-    thank_path # ログアウト後にリダイレクトするパス
-  end
+  # def after_sign_out_path_for(resource_or_scope)
+    # thank_path # ログアウト後にリダイレクトするパス
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-end
+# end
