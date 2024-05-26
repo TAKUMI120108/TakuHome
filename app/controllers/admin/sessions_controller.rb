22ef6c97b544b78e6b9bce0e00cf12537a9f20e5
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-05-26 14:58:55 +0000

    [TakuHome]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -33,4 +33,4 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-# end
+end
