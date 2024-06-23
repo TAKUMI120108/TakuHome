69ea3a9598e1e27c555f703ba656e834b03dc15c
EC2 Default User <ec2-user@ip-172-31-31-30.ap-northeast-1.compute.internal>
2024-06-23 10:36:58 +0000

    [TakuHome]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "jquery";
+import "popper.js";
 import "channels"
 import "bootstrap";
 import "../stylesheets/application.scss";
